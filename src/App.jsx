02bb10b3593538726b259f6402c1d239ec0bc106
k_shivanand <kommagalla.shivanand@p99soft.com>
2026-05-13 22:20:12 +0530

    done2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
-  return (  //counter  done
+  return (  //counter  done done
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Counter: {count}</h1>
 
